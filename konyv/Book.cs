20f6e152169fc0e0aedb69961738c8cc1a3c6b4a
Petrik Diak <diak@petrik.hu>
2025-03-07 12:37:46 +0100

    get bokk kesz?

---
+++ konyv/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyv
+{
+	internal class Book
+	{
+		string title;
+		string author;
+		int pageCount;
+		int releaseYear;
+
+		public Book(string title, string author, int pageCount, int releaseYear)
+		{
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.releaseYear = releaseYear;
+		}
+
+		public string Title { get => title; }
+		public string Author { get => author; }
+		public int PageCount { get => pageCount; }
+		public int ReleaseYear { get => releaseYear; }
+
+		public string GetBookByTitle(Book[] books, string title) 
+		{
+			int asd = 0;
+			for (int i = 0; i < books.Length; i++) 
+			{
+				if (books[i].Title == title)
+				{
+					return $"{books[i].Title} - {books[i].Author} - {books[i].PageCount} - {books[i].ReleaseYear}";
+				}
+				else
+				{
+					asd++;
+				}
+			}
+			if (asd == books.Length) {
+				return "nincs ilyen könyv";
+			}
+			return "";
+		}
+
+	}
+}
